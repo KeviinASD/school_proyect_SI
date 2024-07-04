37e7aaf57459836a3fd1e20970352ea0fdc6ec2a
Kevin Rivas <kevinabril093@gmail.com>
2024-07-04 02:17:45 -0500

    arreglando conflictos v1

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/', function () {return view('welcome');})->name('welcome');
     Route::get('/dashboard', function () {return view('pages.dashboard.index');})->name('dashboard');
+    
 
     Route::get('/gradosYSecciones', [ResumenXController::class, 'index'])->name('gradosYSecciones');
     Route::resource('grados', GradosController::class);
@@ -32,3 +33,9 @@ Route::group(['middleware' => 'guest'], function () {
     Route::post('/login', [AuthController::class, 'postLogin']);
     
 });
+
+
+Route::resource('asignaturas', AsignaturaController::class);
+Route::resource('cursos', CursoController::class);
+Route::delete('/cursos/{idCurso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
+Route::resource('capacidades', CapacidadController::class);
