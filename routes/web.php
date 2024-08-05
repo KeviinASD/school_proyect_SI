2cb850c5c4abfaccd6584db2cff0c3ee908be9a7
01102022 <ivanfer105esq@gmail.com>
2024-08-04 21:40:08 -0500

    actualizacion 04-08-24

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/', function () {return view('welcome');})->name('welcome');
     Route::get('/dashboard', function () {return view('pages.dashboard.index');})->name('dashboard');
-    
-
     Route::get('/gradosYSecciones', [ResumenXController::class, 'index'])->name('gradosYSecciones');
     Route::resource('grados', GradosController::class);
     Route::get('/api/grados/{nivelId}', [GradosController::class, 'getGradosByNivel']);
@@ -34,8 +32,8 @@ Route::group(['middleware' => 'guest'], function () {
     
 });
 
-
 Route::resource('asignaturas', AsignaturaController::class);
 Route::resource('cursos', CursoController::class);
 Route::delete('/cursos/{idCurso}', [CursoController::class, 'destroy'])->name('cursos.destroy');
 Route::resource('capacidades', CapacidadController::class);
+
